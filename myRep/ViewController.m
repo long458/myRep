fb76c974563835b8db26720c3075fe72e61f36b0
ccl_bi <long458@qq.com>
2015-04-14 17:23:55 +0800

    a

--- myRep/ViewController.m
+++ myRep/ViewController.m
@@ -22,7 +22,8 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated
+    
     NSLog(@"this is master didReceiveMemoryWarning");
 }
 
